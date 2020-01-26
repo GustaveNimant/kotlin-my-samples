e399b2f8eebf038086aee73d98bfa3e044401079
Emile Achadde <achadde@iabloko.fecamp>
2020-01-26 11:20:58 +0100

    Lexer_full.kt => Lexer.kt and Mylibrary.kt

--- Lexer_full.kt
+++ Lexer_full.kt
@@ -2,8 +2,6 @@ import java.io.File
 import java.util.Stack
 import java.lang.Character.MIN_VALUE as nullChar
 
-import MyLibrary.*
-
 sealed class Lexeme ()  // Sharped Line 
        	  data class KeywordWithPersonName (val name: String) : Lexeme ()
 	  data class KeywordWithDate (val name: String) : Lexeme ()
@@ -52,6 +50,205 @@ sealed class Lexeme ()  // Sharped Line
 
 data class pairString (val first: String, val second: String)
 
+// beginning of library
+
+val debug = false
+var level = 0
+var dots = "........|........|........|........|........|........|........|"
+
+fun entering(here:String, caller:String):Unit {
+    level = level + 1
+    if (level > 70) {
+       println ("Errot maximum number of nesting levels reached")
+    } else {
+        var points = dots.substring(0, level)
+        println("$points Entering  in $here called by $caller")
+    }
+}
+
+fun exiting(here:String):Unit {
+    var points = dots.substring(0, level)
+    println("$points Exiting from $here")
+    level = level - 1	
+}
+
+fun functionName():String {
+    val sta = Thread.currentThread().stackTrace[2]
+    val str = sta.getMethodName()
+    return str	
+}
+
+fun countOfCharOfString (cha: Char, str:String, caller:String) : Int {
+    val here = functionName()
+    entering(here, caller)
+
+    var count = 0
+
+    for (c in str) {
+        if (c == cha){count = count + 1}
+    }	
+		
+    exiting(here + " with count " + count.toString())
+    return count
+}
+
+fun lineListOfFileName (nof: String, caller: String) : MutableList<String> {
+    val here = functionName()
+    entering(here, caller)
+
+    println("$here: input nof '$nof'")
+
+    val result = mutableListOf<String>()
+ 
+    File(nof).useLines {
+    	lines -> lines.forEach { result.add(it)}
+	}
+
+  println ("$here: output result '$result'")	
+  exiting(here)
+  return result
+}
+
+fun notYetImplemented(fun_nam: String){
+    throw Exception("Error: function '$fun_nam' is not yet implemented")
+}
+
+fun read_input(caller:String):String {
+    val here = functionName()
+    entering(here, caller)
+	
+    val str = readLine().toString()
+    
+    exiting(here)
+    return str
+}
+
+fun write_output(fileName:String, content: String, caller:String) {
+    val here = functionName()
+    entering(here, caller)
+	
+    File(fileName).bufferedWriter().use { out -> out.write(content)}
+    
+    exiting(here)
+}
+
+fun nextWordOfString(pos:Int, lin: String, caller: String): String {
+    val here = functionName()
+    entering(here, caller)
+
+    println("$here: input pos '$pos'")
+    println("$here: input lin '$lin'")
+    
+    val str = lin.substring(pos)
+    var word = ""    
+    for (c in str){
+	  println("$here: c '$c'")
+	  if (isTokenOfChar(c, here)) {break}
+	  word = word.plus(c.toString())
+    }
+
+    assert (word.isNotEmpty())
+    
+    println("$here: output word '$word'")
+    exiting(here)
+    return word
+}
+
+fun nextWordOfEndCharOfString(del: Char, str: String, caller: String): String {
+    val here = functionName()
+    entering(here, caller)
+
+    println("$here: input del '$del'")
+    println("$here: input str '$str'")
+    
+    var word = ""    
+    for (c in str){
+	  println("$here: c '$c'")
+	  if (c.equals(del)) {break}
+	  word = word.plus(c.toString())
+    }
+
+    assert (word.isNotEmpty())
+    
+    println("$here: output word '$word'")
+    exiting(here)
+    return word
+}
+
+fun lineStackOfLineList (str_l: List<String>) : Stack<String> {
+    var stack = Stack<String>()
+    str_l.reversed().forEach { l -> stack.push(l) }
+    return stack
+}
+
+fun wordListOfString (str: String): List<String> {
+    val trimedString = str.trim(' ')    
+    val regex = Regex("""\s+""")
+
+    val result = trimedString.split(regex)
+
+    return result
+}
+
+fun wordStackOfLine (lin: String) : Stack<String> {
+    var stack = Stack<String>()
+    var wor_l = wordListOfString (lin)
+    wor_l.reversed().forEach { w -> stack.push(w)}
+    return stack
+}
+
+fun isAlphabeticalOfChar(cha: Char, caller: String): Boolean {
+    val here = functionName()
+    entering(here, caller)
+    
+    val pattern = Regex("[a-zA-Z_]")
+    println("$here: input cha '$cha'")
+    val result = pattern.matches(cha.toString())
+
+    exiting(here + " with result '$result'")
+    return result
+}
+
+fun isAlphanumericalOfChar(cha: Char, caller: String): Boolean {
+    val here = functionName()
+    entering(here, caller)
+    
+    val pattern = Regex("[a-zA-Z_0-9]")
+    println("$here: input cha '$cha'")
+    val result = pattern.matches(cha.toString())
+
+    exiting(here + " with result '$result'")
+    return result
+}
+
+fun isNumericalOfChar(cha: Char, caller: String): Boolean {
+    val here = functionName()
+    entering(here, caller)
+    
+    val pattern = Regex("[0-9]")
+    println("$here: input cha '$cha'")
+    val result = pattern.matches(cha.toString())
+
+    exiting(here + " with result '$result'")
+    return result
+}
+
+fun stringListOfLexemeList (lex_l: List<Lexeme>) : List<String> {
+ val str_l = lex_l.map({l -> stringOfLexeme (l) })
+ return str_l 
+}
+
+fun stringOfGlueOfStringList (glue: String, str_l: List<String>) : String {
+ val str = str_l.fold("", {acc, s -> acc + s + glue })
+ return str 
+}
+
+fun stringOfStringList (str_l: List<String>) : String {
+ val str = str_l.fold("", {acc, s -> acc + s })
+ return str 
+}
+
+// end of library
 
 fun isAuthorNameOfString(str: String, caller: String): Boolean {
     val here = functionName()
@@ -1245,28 +1442,6 @@ fun lexemeListOfTicLine (lin: String, caller:String) : MutableList<Lexeme> {
    return lexemeList
 }
 
-fun nextWordOfString(pos:Int, lin: String, caller: String): String {
-    val here = functionName()
-    entering(here, caller)
-
-    println("$here: input pos '$pos'")
-    println("$here: input lin '$lin'")
-    
-    val str = lin.substring(pos)
-    var word = ""    
-    for (c in str){
-	  println("$here: c '$c'")
-	  if (isTokenOfChar(c, here)) {break}
-	  word = word.plus(c.toString())
-    }
-
-    assert (word.isNotEmpty())
-    
-    println("$here: output word '$word'")
-    exiting(here)
-    return word
-}
-
 fun stringOfLexeme (lexeme: Lexeme): String {
     val string = when (lexeme) {
         is KeywordWithPersonName -> "KeywordWithPersonName("+lexeme.name+")"
@@ -1311,11 +1486,6 @@ is TokenNumerical -> "TokenNumerical("+lexeme.character.toString()+")"
 
 }
     return string
-}
-
-fun stringListOfLexemeList (lex_l: List<Lexeme>) : List<String> {
- val str_l = lex_l.map({l -> stringOfLexeme (l) })
- return str_l 
 }
 
 fun tokenOfChar(cha: Char, pos: Int, lin: String, caller: String) : Lexeme {
--- MyLibrary.kt
+++ MyLibrary.kt
@@ -2,16 +2,16 @@ package MyLibrary
 
 import java.io.File
 import java.util.Base64
+import java.util.Stack
+import java.lang.Character.MIN_VALUE as nullChar
 
 class MyLibrary
+
+// beginning of library
+
+val debug = false
 var level = 0
 var dots = "........|........|........|........|........|........|........|"
-
-fun functionName():String {
-    val sta = Thread.currentThread().stackTrace[2]
-    val str = sta.getMethodName()
-    return str	
-}
 
 fun entering(here:String, caller:String):Unit {
     level = level + 1
@@ -29,11 +29,48 @@ fun exiting(here:String):Unit {
     level = level - 1	
 }
 
+fun functionName():String {
+    val sta = Thread.currentThread().stackTrace[2]
+    val str = sta.getMethodName()
+    return str	
+}
+
+fun countOfCharOfString (cha: Char, str:String, caller:String) : Int {
+    val here = functionName()
+    entering(here, caller)
+
+    var count = 0
+
+    for (c in str) {
+        if (c == cha){count = count + 1}
+    }	
+		
+    exiting(here + " with count " + count.toString())
+    return count
+}
+
+fun lineListOfFileName (nof: String, caller: String) : MutableList<String> {
+    val here = functionName()
+    entering(here, caller)
+
+    println("$here: input nof '$nof'")
+
+    val result = mutableListOf<String>()
+ 
+    File(nof).useLines {
+    	lines -> lines.forEach { result.add(it)}
+	}
+
+  println ("$here: output result '$result'")	
+  exiting(here)
+  return result
+}
+
 fun notYetImplemented(fun_nam: String){
     throw Exception("Error: function '$fun_nam' is not yet implemented")
 }
 
-fun inputRead(caller:String):String {
+fun read_input(caller:String):String {
     val here = functionName()
     entering(here, caller)
 	
@@ -43,7 +80,7 @@ fun inputRead(caller:String):String {
     return str
 }
 
-fun outputWrite(fileName:String, content: String, caller:String) {
+fun write_output(fileName:String, content: String, caller:String) {
     val here = functionName()
     entering(here, caller)
 	
@@ -52,3 +89,93 @@ fun outputWrite(fileName:String, content: String, caller:String) {
     exiting(here)
 }
 
+fun nextWordOfEndCharOfString(del: Char, str: String, caller: String): String {
+    val here = functionName()
+    entering(here, caller)
+
+    println("$here: input del '$del'")
+    println("$here: input str '$str'")
+    
+    var word = ""    
+    for (c in str){
+	  println("$here: c '$c'")
+	  if (c.equals(del)) {break}
+	  word = word.plus(c.toString())
+    }
+
+    assert (word.isNotEmpty())
+    
+    println("$here: output word '$word'")
+    exiting(here)
+    return word
+}
+
+fun lineStackOfLineList (str_l: List<String>) : Stack<String> {
+    var stack = Stack<String>()
+    str_l.reversed().forEach { l -> stack.push(l) }
+    return stack
+}
+
+fun wordListOfString (str: String): List<String> {
+    val trimedString = str.trim(' ')    
+    val regex = Regex("""\s+""")
+
+    val result = trimedString.split(regex)
+
+    return result
+}
+
+fun wordStackOfLine (lin: String) : Stack<String> {
+    var stack = Stack<String>()
+    var wor_l = wordListOfString (lin)
+    wor_l.reversed().forEach { w -> stack.push(w)}
+    return stack
+}
+
+fun isAlphabeticalOfChar(cha: Char, caller: String): Boolean {
+    val here = functionName()
+    entering(here, caller)
+    
+    val pattern = Regex("[a-zA-Z_]")
+    println("$here: input cha '$cha'")
+    val result = pattern.matches(cha.toString())
+
+    exiting(here + " with result '$result'")
+    return result
+}
+
+fun isAlphanumericalOfChar(cha: Char, caller: String): Boolean {
+    val here = functionName()
+    entering(here, caller)
+    
+    val pattern = Regex("[a-zA-Z_0-9]")
+    println("$here: input cha '$cha'")
+    val result = pattern.matches(cha.toString())
+
+    exiting(here + " with result '$result'")
+    return result
+}
+
+fun isNumericalOfChar(cha: Char, caller: String): Boolean {
+    val here = functionName()
+    entering(here, caller)
+    
+    val pattern = Regex("[0-9]")
+    println("$here: input cha '$cha'")
+    val result = pattern.matches(cha.toString())
+
+    exiting(here + " with result '$result'")
+    return result
+}
+
+fun stringOfGlueOfStringList (glue: String, str_l: List<String>) : String {
+ val str = str_l.fold("", {acc, s -> acc + s + glue })
+ return str 
+}
+
+fun stringOfStringList (str_l: List<String>) : String {
+ val str = str_l.fold("", {acc, s -> acc + s })
+ return str 
+}
+
+// end of library
